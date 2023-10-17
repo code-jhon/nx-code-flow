fb93ace8d1df6ef45f235989758891de6b24b2d3
jhon avila <javilaortiz@gmail.com>
2023-10-17 18:05:55 -0500

    feat: template, page and header done

---
+++ apps/realstate-front/src/app/components/pages/NewPassword/NewPassword.tsx
@@ -0,0 +1,12 @@
+import { NewPasswordTemplate } from "../../templates";
+import { VerifyCodeForm } from "../../organisms/"
+
+const NewPassword = () => {
+  return (
+    <NewPasswordTemplate>
+      <VerifyCodeForm />
+    </NewPasswordTemplate>
+  )
+}
+
+export default NewPassword 
--- apps/realstate-front/src/app/components/templates/index.tsx
+++ apps/realstate-front/src/app/components/templates/index.tsx
@@ -1,9 +1,11 @@
 import { SignInPage } from "./SignInPage/SignInPage";
 import { SignUpPage } from "./SignUpPage/SignUpPage";
 import { PasswordRecoveryPage } from "./PasswordRecoveryPage/PasswordRecoveryPage";
+import { NewPasswordPage } from "./NewPasswordPage/NewPasswordPage";
 
 export { 
   SignInPage as SignInTemplate 
 , SignUpPage as SignUpTemplate
 , PasswordRecoveryPage as PasswordRecoveryTemplate
+, NewPasswordPage as NewPasswordTemplate
 }
