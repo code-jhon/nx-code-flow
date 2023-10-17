14df6a06317efa18e8ed0f659efea5f9719fe09a
jhon avila <javilaortiz@gmail.com>
2023-10-16 21:30:05 -0500

    feat: adding Img atom and his implementation in footer

--- apps/realstate-front/src/app/components/atoms/index.tsx
+++ apps/realstate-front/src/app/components/atoms/index.tsx
@@ -1,5 +1,6 @@
 import Button from './Button/Button';
 import Input from './Input/Input';
 import Text from './Text/Text';
+import Img from './Img/Img';
 
-export { Button, Input, Text };
+export { Button, Input, Text, Img };
--- apps/realstate-front/src/app/components/organisms/Footer/Footer.tsx
+++ apps/realstate-front/src/app/components/organisms/Footer/Footer.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
+import { Text, Img } from "../../atoms";
+import AppleIcon from '../../../../assets/icons/apple-icon.svg';
+import GoogleIcon from '../../../../assets/icons/google-icon.svg';
+import FacebookIcon from '../../../../assets/icons/facebook-icon.svg';
 
 export const Footer:React.FC = () => {
   return (
     <>
       <div className="flex justify-center mt-4">
-        <p className="text-gray-600">Or sign in with</p>
+        <Text textType="p" className="text-gray-600">Or sign in with</Text>
       </div>
       
-      <div className="flex justify-center mt-2">
-        {/* Replace with your social icons */}
-        <span className="w-8 h-8 bg-gray-300 rounded-full mx-2"></span>
-        <span className="w-8 h-8 bg-gray-300 rounded-full mx-2"></span>
-        <span className="w-8 h-8 bg-gray-300 rounded-full mx-2"></span>
+      <div className="flex justify-center mt-2 p-4">
+        <Img src={AppleIcon} className="w-10 h-10 rounded-full mx-4 p-2 hover:scale-105 cursor-pointer border-solid border-slate-950" alt="Apple Icon" />
+        <Img src={GoogleIcon} className="w-10 h-10 rounded-full mx-4 p-2 hover:scale-105 cursor-pointer border-solid border-slate-950" alt="Google Icon" />
+        <Img src={FacebookIcon} className="w-10 h-10 rounded-full mx-4 p-2 hover:scale-105 cursor-pointer border-solid border-slate-950" alt="Facebook Icon" />
       </div>
       
       <div className="flex justify-center mt-4">
-        <p className="text-gray-600">Don't have an account? <span className="text-blue-500 hover:underline cursor-pointer">Sign Up</span></p>
+        <Text textType="p" className="text-gray-600">Don't have an account? <span className="text-blue-500 hover:underline cursor-pointer">Sign Up</span></Text>
       </div>
     </>
   )
